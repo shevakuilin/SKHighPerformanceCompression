c02b66a38ccbf2adc80018e5436fdba3a2c8025b
shevakuilin <shevakuilin@gmail.com>
2017-11-17 17:51:23 +0800

    creat animation picture code

--- SKHighPerformanceCompression/SKHighPerformanceCompression/MainInterface/SKMainInterfaceViewController.swift
+++ SKHighPerformanceCompression/SKHighPerformanceCompression/MainInterface/SKMainInterfaceViewController.swift
@@ -12,6 +12,11 @@ import Cocoa
 
 class SKMainInterfaceViewController: NSViewController {
 
+    @IBOutlet weak var settingButton: NSButton!
+    @IBOutlet weak var reminderInfoLabel: NSTextField!
+    @IBOutlet weak var applicationTitleLabel: NSTextField!
+    @IBOutlet weak var animationImageView: NSImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setBackground()
@@ -29,7 +34,18 @@ extension SKMainInterfaceViewController {
     // MARK: Set background (e.g., view's backgroundColor, defaultTitle's textColor..)
     private func setBackground() {
         self.view.wantsLayer = true // Open the layer draw
-        self.view.layer?.backgroundColor = NSColor(red: 37/255.0, green: 37/255.0, blue: 37/255.0, alpha: 1.0).cgColor
+        self.view.layer?.backgroundColor = NSColor(red: 52/255.0, green: 52/255.0, blue: 52/255.0, alpha: 1.0).cgColor
+        
+        // Read animation pictures
+        var pictureFrames:Array<String> = []
+        let picturePrefix = "Hover+compress+gift+relax_"
+        for i in 44..<172 {
+            let pictureSuffix = i < 100 ? "000" + "\(i)" : "00" + "\(i)"
+            let pictureFull = picturePrefix + pictureSuffix
+            pictureFrames.append(pictureFull)
+        }
+        
+        // Perform the animation
     }
     
 }
@@ -38,9 +54,9 @@ extension SKMainInterfaceViewController {
     
     // MARK: Set basics interaction action (e.g., drag and drop gesture)
     private func setBasicsInteractionAction() {
-        // Get view's window
+        // Get & set view's window
         let basicsWindows = self.view.window
-        basicsWindows?.isMovableByWindowBackground = true // Allows the mouse to drag and drop the background movement
+        basicsWindows?.isMovableByWindowBackground = true // Allows the mouse to drag & drop the background movement
     }
     
 }
